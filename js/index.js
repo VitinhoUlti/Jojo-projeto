1ed5f4e9206f19cd2867fb84fd98dabbe1204968
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2023-09-14 02:35:59 -0300

    primeiro funcionamento

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let imagem = window.document.getElementById("hea")
+function aperte() {
+    imagem.style.background = `url(imagens/alljoestar.jpg)`
+    window.document.body.style.backgroundColor = 'darksalmon'
+}
+function aperte2() {
+    imagem.style.background = `url(imagens/allvillains.webp)`
+    imagem.style.backgroundPosition = "center top"
+    imagem.style.backgroundSize = "cover"
+    window.document.body.style.backgroundColor = 'rgb(66, 64, 64)'
+}
