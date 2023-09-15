2652ef5f008145c8746b45fda831db7392130086
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2023-09-15 00:03:27 -0300

    mais texto

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 let imagem = window.document.getElementById("hea")
+let imagem2 = window.document.getElementById("pers1")
 function aperte() {
     imagem.style.background = `url(imagens/alljoestar.jpg)`
+    imagem2.style.background = `url(imagens/jojo-s-bizarre-adventure-stone-ocean_cke.webp)`
+    imagem2.style.backgroundPosition = "center top"
+    imagem2.style.backgroundSize = "cover"
     window.document.body.style.backgroundColor = 'darksalmon'
 }
 function aperte2() {
     imagem.style.background = `url(imagens/allvillains.webp)`
     imagem.style.backgroundPosition = "center top"
     imagem.style.backgroundSize = "cover"
+    imagem2.style.background = `url(imagens/joji.avif)`
+    imagem2.style.backgroundPosition = "center top"
+    imagem2.style.backgroundSize = "cover"
     window.document.body.style.backgroundColor = 'rgb(66, 64, 64)'
 }
